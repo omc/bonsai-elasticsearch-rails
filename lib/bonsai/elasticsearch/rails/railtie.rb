d20e0905558af5c708e431652cb1da76241794be
Maddie Jones <maddiejonesg@gmail.com>
2019-11-04 11:13:11 -0700

    Adds gem testing

--- lib/bonsai/elasticsearch/rails/railtie.rb
+++ lib/bonsai/elasticsearch/rails/railtie.rb
@@ -7,23 +7,28 @@ module Bonsai
           require 'elasticsearch/model'
           require 'elasticsearch/rails'
 
-          logger = ::Rails.logger
+          # Print a debug message to STDOUT. Muted when in a test environment.
+          def log(message)
+            @@logger.debug(message) unless ::Rails.env.test?
+          end
+
+          @@logger = Logger.new(STDOUT)
           url = ENV['BONSAI_URL'] || ENV['ELASTICSEARCH_URL']
 
           begin
             if url && URI.parse(url)
               filtered_url = url.sub(/:[^:@]+@/, ':FILTERED@')
-              logger.debug('Bonsai: Initializing default Elasticsearch client'\
+              log('Bonsai: Initializing default Elasticsearch client'\
                            " with #{filtered_url}")
               ::Elasticsearch::Model.client = ::Elasticsearch::Client.new(
                 url: url
               )
             elsif ::Rails.env.production?
-              logger.debug('BONSAI_URL not present, proceeding with'\
+              log('BONSAI_URL not present, proceeding with'\
                            'Elasticsearch defaults.')
             end
           rescue URI::InvalidURIError => e
-            logger.error("Elasticsearch cluster URL is invalid: #{e.message}")
+            log("Elasticsearch cluster URL is invalid: #{e.message}")
           end
         end
       end
---
+++ spec/railtie_spec.rb
@@ -0,0 +1,54 @@
+require 'rails'
+require_relative '../lib/bonsai/elasticsearch/rails/railtie'
+require 'pry'
+
+describe Bonsai::Elasticsearch::Rails::Railtie do
+  let(:initializer) do
+   described_class.initializers.select{|i| i.name == 'setup_elasticsearch' }.first
+  end
+
+  let(:bonsai_url) { "https://user123:pass456@some-cluster.bonsai.io:443" }
+
+  let(:cluster_url) { "http://127.0.0.1" }
+
+  let(:client_url) { ::Elasticsearch::Model.client.transport.options[:url] }
+
+  let(:simulate_rails_startup) { initializer.block.call }
+
+  before do
+    ENV['RAILS_ENV'] = 'test'
+  end
+
+  it 'has the right initializer' do
+    expect(initializer).not_to be_nil
+  end
+
+  describe 'URL assignment' do
+    before do
+      ENV['BONSAI_URL'] = nil
+    end
+
+    context 'when no environment variable exists' do
+      it 'defaults to whatever Elasticsearch-rails wants' do
+        simulate_rails_startup
+        expect(client_url).to be_nil
+      end
+    end
+
+    context 'when BONSAI_URL exists' do
+      it 'sets the client to the Bonsai URL' do
+        ENV['BONSAI_URL'] = bonsai_url
+        simulate_rails_startup
+        expect(client_url).to eql(bonsai_url)
+      end
+    end
+
+    context 'when ELASTICSEARCH_URL exists' do
+      it 'sets the client to the cluster url' do
+        ENV['ELASTICSEARCH_URL'] = cluster_url
+        simulate_rails_startup
+        expect(client_url).to eql(cluster_url)
+      end
+    end
+  end
+end
